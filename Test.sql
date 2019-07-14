b3859c17022e1fd5c8b6517dc13943fdbe950fec
subway00 <pig945513@gmail.com>
2019-07-15 02:58:03 +0800

    Update: new F_able T_able.

--- Test.sql
+++ Test.sql
@@ -1,12 +1,19 @@
-SELECT * FROM Member
+SELECT * FROM FileFolder
 INSERT INTO Member (M_Email, M_Born) VALUES ( '12345'  , '1400-10-10' )
 INSERT INTO Member (M_Email, M_Born, M_Gender, M_PW) VALUES ( '12345'  , '1400-10-10', 'F', 'a13334' )
 --顯示資料結構
-exec sp_columns Test
+exec sp_columns FileFolder
 GO
 --重製識別碼
-DBCC CHECKIDENT (Member, RESEED, 1)
+DBCC CHECKIDENT (FileFolder, RESEED, 0)
 --清空資料表內容
-DELETE  FROM  MEMBER
+DELETE  FROM  FileFolder
 
 SELECT M_Email, M_PW FROM Member
+
+INSERT INTO FileFolder (F_Name, F_Able) VALUES ('789',  0)
+INSERT INTO Test (T_Able) VALUES (1)
+
+UPDATE FileFolder SET F_Able=0 WHERE F_Name='11'
+
+SELECT F_Name FROM FileFolder WHERE F_Able=1 ORDER BY F_Number
