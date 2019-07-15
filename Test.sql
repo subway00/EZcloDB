90dcf557a0c5f64e28e6e2aa9253a373f00c21aa
subway00 <pig945513@gmail.com>
2019-07-16 06:45:47 +0800

    no message

--- Test.sql
+++ Test.sql
@@ -1,6 +1,4 @@
 SELECT * FROM FileFolder
-INSERT INTO Member (M_Email, M_Born) VALUES ( '12345'  , '1400-10-10' )
-INSERT INTO Member (M_Email, M_Born, M_Gender, M_PW) VALUES ( '12345'  , '1400-10-10', 'F', 'a13334' )
 --顯示資料結構
 exec sp_columns Member
 GO
@@ -8,6 +6,9 @@ GO
 DBCC CHECKIDENT (FileFolder, RESEED, 0)
 --清空資料表內容
 DELETE  FROM  FileFolder
+------------------------------------------------
+INSERT INTO Member (M_Email, M_Born) VALUES ( '12345'  , '1400-10-10' )
+INSERT INTO Member (M_Email, M_Born, M_Gender, M_PW) VALUES ( '12345'  , '1400-10-10', 'F', 'a13334' )
 
 SELECT M_Email, M_PW FROM Member
 
@@ -17,3 +18,5 @@ INSERT INTO Test (T_Able) VALUES (1)
 UPDATE FileFolder SET F_Able=0 WHERE F_Name='11'
 
 SELECT F_Name FROM FileFolder WHERE F_Able=1 ORDER BY F_Number
+
+UPDATE FileFolder SET F_Name='999' WHERE F_Name='789' AND F_Able='1'
