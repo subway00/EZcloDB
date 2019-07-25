e0bd83e5c16eaad02046f73263da6a498d159b2b
subway <pig945513@gmail.com>
2019-07-25 17:13:44 +0800

    Upload!

--- Test.sql
+++ Test.sql
@@ -1,6 +1,8 @@
-SELECT * FROM Test
+SELECT * FROM Result
+SELECT * FROM Member
+
 --顯示資料結構
-exec sp_columns Test
+exec sp_columns Result
 GO
 --重製識別碼
 DBCC CHECKIDENT (FileFolder, RESEED, 0)
@@ -9,9 +11,18 @@ DELETE  FROM  FileFolder
 ------------------------------------------------
 --新增欄位
 ALTER TABLE MEMBER ADD [M_Active] [bit] NULL
+----試卷資訊
+SELECT T_Name, T.T_Number, T_BuildTime, R_TestTime 
+FROM FileFolder AS F LEFT JOIN Test AS T
+ON F.F_Number = T.F_Number
+LEFT JOIN Result AS R
+ON T.T_Number = R.T_Number
+WHERE F.F_Number=4051
+---
+ALTER TABLE Result ALTER COLUMN R_TestTime date
 INSERT INTO Member (M_Email, M_Born) VALUES ( '12345'  , '1400-10-10' )
 INSERT INTO Member (M_Email, M_Born, M_Gender, M_PW) VALUES ( '12345'  , '1400-10-10', 'F', 'a13334' )
-
+INSERT INTO Result (R_Right, R_Wrong, T_Number, F_Number
 SELECT M_Email, M_PW FROM Member
 
 INSERT INTO FileFolder (F_Name, F_Able) VALUES ('789',  0)
@@ -30,3 +41,15 @@ SELECT T_Number FROM Test WHERE T_Able=1 AND T_Name='6'
 UPDATE Test SET T_Name='Fkj' WHERE T_Number=10
 UPDATE Test SET T_Name='qwerasd' WHERE T_Number=8 AND T_Able='1'
 UPDATE Member SET M_Active=1 WHERE M_Number=1
+
+INSERT INTO Test(T_Name) VALUES ('yyyee')
+SELECT * FROM Test WHERE T_Name='yyyee'
+----帳號資訊
+SELECT M_Email, M_Gender, M_Born  
+FROM Member AS M LEFT JOIN FileFolder AS F
+ON M.M_Number=F.M_Number
+LEFT JOIN Test AS T
+ON F.F_Number=T.F_Number
+LEFT JOIN Result AS R
+ON T.T_Number=R.T_Number
+WHERE M.M_Number=2003 
